c9937e39be9b4438372a05d0e1f178aa0a321277
Roberto <roberto3154_421@hotmail.com>
2022-10-07 12:10:53 -0300

    Create pokemonTypeColors.dart

---
+++ lib/domain/pokemonTypeColors.dart
@@ -0,0 +1,24 @@
+import 'dart:ui';
+
+class PokemonTypeColors {
+  final Map<String, Color> colorByType = {
+    'normal': const Color.fromARGB(255, 168, 167, 122),
+    'fire': const Color.fromARGB(255, 238, 129, 48),
+    'water': const Color.fromARGB(255, 99, 144, 240),
+    'electric': const Color.fromARGB(255, 247, 208, 44),
+    'grass': const Color.fromARGB(255, 122, 199, 76),
+    'ice': const Color.fromARGB(255, 150, 217, 214),
+    'fighting': const Color.fromARGB(255, 194, 46, 40),
+    'poison': const Color.fromARGB(255, 163, 62, 161),
+    'ground': const Color.fromARGB(255, 226, 191, 101),
+    'flying': const Color.fromARGB(255, 169, 143, 243),
+    'psychic': const Color.fromARGB(255, 249, 85, 135),
+    'bug': const Color.fromARGB(255, 166, 185, 26),
+    'rock': const Color.fromARGB(255, 182, 161, 54),
+    'ghost': const Color.fromARGB(255, 115, 87, 151),
+    'dragon': const Color.fromARGB(255, 111, 53, 252),
+    'dark': const Color.fromARGB(255, 112, 87, 70),
+    'steel': const Color.fromARGB(255, 183, 183, 206),
+    'fairy': const Color.fromARGB(255, 214, 133, 173),
+  };
+}
