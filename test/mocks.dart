3abfde1df866924d4f5c161d0058f86258f20f5f
Roberto <roberto3154_421@hotmail.com>
2023-02-05 17:53:53 -0300

    feat: create use cases mocks

---
+++ test/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:pokedex/domain/use_case/number_pages_use_case.dart';
+import 'package:pokedex/domain/use_case/pokemon_list_use_case.dart';
+
+class PokemonListUseCaseMock extends Mock implements PokemonListUseCase {}
+class NumberPagesUseCaseMock extends Mock implements NumberPagesUseCase {}
